01ea6d4f9f1ba757eda20723f48e4501fc674039
Oksana Alekseienko <oksana.alekseienko@gmail.com>
2020-04-22 18:40:19 +0300

    test

---
+++ connect2.php
@@ -0,0 +1,18 @@
+<?php
+
+try {
+    $dbh = new PDO('mysql:host=127.0.0.1;port=3307;dbname=db_oksana', 'oksana', 's*5zSIo7hAoM4vOaxO9E2jL7XCaCamsK1');
+    foreach($dbh->query('SELECT * from users', PDO::FETCH_ASSOC) as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+catch (Exception $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
+
